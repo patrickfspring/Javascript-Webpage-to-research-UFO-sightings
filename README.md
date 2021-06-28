8d3130b8c76f59e91347c2e5fa5de34bb6b2bcd4
unknown <pfspring84@gmail.com>
2021-06-27 20:27:07 -0500

    Sunday evening commits- cleanup and readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # javascript-challenge
 javascript challenge
+
+The purpose of this project is to be able to dislplay a table of reported UFO sitings and be able to search the sightings by date.
+
+The project contents are the following:
+(1.) Static file with the CSS and images subfolders.  CSS content was updated for this homework project.
+(2.) The js folder with the data and app scripts. 
+(3.) The index html file, which was also updated for this homework project. 
+(4.) The UFO-level-2 file was not used due to time constraints.
--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -6,11 +6,10 @@ function initialLoad() {
   var tableContent = "";
   var columns = [];
 
-  // Create the header for the table
-
+  // Create the header for the table.
   for(var prop in tableData[0]) {
     if(tableData[0].hasOwnProperty(prop)) {
-    // Keep the columns for later to get column values from the 'data' object.
+    // Keep the columns for later reference to get the column values from the 'data' object.
        columns.push(prop);
     };
   };
@@ -35,32 +34,30 @@ function initialLoad() {
 
 }  
 
-// Complete the event handler function for the form
+// Complete the event handler function to filter the entire dataset.
 function runFilteredList() {
 
-  // Prevent the page from refreshing
+  // Prevent the page from refreshing.
   d3.event.preventDefault();
-  //console.log("here2");
   
-  // Select the input element and getg the raw HTML node
+  // Select the input element and get the raw HTML node.
   var inputElement = d3.select("#datetime");
 
-  // Get the value property of the input element
+  // Get the value property of the input element.
   var inputValue = inputElement.property("value");
-  console.log(inputValue);
+  //console.log(inputValue);
 
   var filteredEvents = tableData.filter(tableRow => tableRow.datetime === inputValue)
 
-  console.log(filteredEvents);
+  //console.log(filteredEvents);
   
   var tableContent = "";
   var columns = [];
 
-  // Create the header for the table
-
+  // Create the header for the table.
   for(var prop in filteredEvents[0]) {
     if(filteredEvents[0].hasOwnProperty(prop)) {
-    // Keep the columns for later to get column values from the 'data' object.
+    // Keep the columns for later reference to get the column values from the 'data' object.
       columns.push(prop);
     };
   };
@@ -81,19 +78,19 @@ function runFilteredList() {
   });
 
     //console.log(tableContent);
+    // Clear the previous content from the body portion of the table. 
     $("#ufo-table").find("tbody").empty();
     $("#ufo-table").append(tableContent);
 
 };
 
-// Select the filter button
+// Select the filter button.
 var button = d3.select("#filter-btn");
-// Select the form
+// Select the form.
 var form = d3.select("#form");
 
-// Create event handler
+// Pass control to functions based event handlers
 button.on("click", runFilteredList);
 form.on("submit", runFilteredList);
 
-//console.log("here4");
 initialLoad();
